e4bb3b2fc32d04eb8ff7ef9ba74b406301da873a
Yang Jia <iatbst@gmail.com>
2022-02-28 18:11:54 +0800

    -

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -56,7 +56,7 @@
         <!-- 仓位排名 -->
         <position-ranks v-bind:subaccount-datas="subaccountDatas" v-if="subaccountDatasAvailable"></position-ranks> 
         <!-- 仓位详情 -->
-        <div style="width: 95%">
+        <div style="width: 95%" align="center">
             <position-map 
             v-bind:positions="positions" 
             v-bind:positions-loading="positionsLoading"
