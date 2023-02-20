0c2d90084e4d0bbea7c96800ff736ebb0ab7bc73
Yang Jia <iatbst@gmail.com>
2023-02-20 18:58:16 +0800

    -

--- src/configs/system_configs.js
+++ src/configs/system_configs.js
@@ -1,6 +1,5 @@
 // Master的Host地址
 const masterHost = 'http://ec2-18-138-152-197.ap-southeast-1.compute.amazonaws.com:8000/api'
-// const masterHost = 'http://18.138.152.197:8000/api'
 
 // 当前投资组合的Hosts地址(由环境变量VUE_APP_PFO_HOSTS决定)
 if (process.env.VUE_APP_PFO_HOSTS === ''){
@@ -12,7 +11,7 @@ const pfoHosts = pfoHostList
 
 // Backtest Host地址(运行回测的Host)
 // const backtestHost = 'http://localhost:8000/api'
-const backtestHost = 'http://ec2-13-214-131-132.ap-southeast-1.compute.amazonaws.com:8000/api'
+const backtestHost = 'http://ec2-54-169-125-145.ap-southeast-1.compute.amazonaws.com:8000/api'
 
 // 当前投资组合的别名(product)
 const pfoAlias = [
