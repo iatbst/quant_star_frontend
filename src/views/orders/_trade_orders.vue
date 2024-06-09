bdfeed3cdc9ec99d22186d80395467780f7a8188
Yang Jia <iatbst@gmail.com>
2024-06-10 01:04:32 +0800

    -

--- src/views/orders/_trade_orders.vue
+++ src/views/orders/_trade_orders.vue
@@ -188,11 +188,11 @@
                             {{ chineseString(scope.row.status) }}
                             </span>
                             <span style="color: red" v-else>
-                            {{ chineseString(scope.row.status) }}
+                            {{ chineseString(scope.row.status) }}({{ (scope.row.exec_size*100/scope.row.size).toFixed(0) }}%)
                             </span>
                         </template>
                     </el-table-column>
-                    
+
                     <el-table-column align="center" label="成交额" min-width="10%">
                         <template slot-scope="scope">
                             <div v-if="scope.row.volume !== null">
