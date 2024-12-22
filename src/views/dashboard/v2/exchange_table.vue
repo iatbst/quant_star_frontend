d5d0f0d11dcf6d8b605d608881fe4675301d6c6b
Yang Jia <iatbst@gmail.com>
2024-12-23 01:13:41 +0800

    -

---
+++ src/views/dashboard/v2/exchange_table.vue
@@ -0,0 +1,188 @@
+<template>
+    <!--- 表3: 平台 --->
+    <div>
+        <el-table
+        :data="exchangeDatas"
+        :header-cell-style="{ background: '#f2f2f2' }"
+        style="width: 100%">
+            <el-table-column label="BINANCE资金(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="">
+                        {{toThousands(Math.round(scope.row.binanceBalance/1000))}}
+                    </span>              
+                </template>
+            </el-table-column>
+
+            <el-table-column label="BINANCE仓位(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="color: green" v-if="scope.row.binancePosition >= 0">
+                        {{toThousands(Math.round(scope.row.binancePosition/1000))}}
+                    </span>   
+                    <span style="color: red" v-else>
+                        {{toThousands(Math.round(scope.row.binancePosition/1000))}}
+                    </span>             
+                </template>   
+            </el-table-column>
+
+            <el-table-column label="OKEX资金(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="">
+                        {{toThousands(Math.round(scope.row.okexBalance/1000))}}
+                    </span>              
+                </template>
+            </el-table-column>
+
+            <el-table-column label="OKEX仓位(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="color: green" v-if="scope.row.okexPosition >= 0">
+                        {{toThousands(Math.round(scope.row.okexPosition/1000))}}
+                    </span>   
+                    <span style="color: red" v-else>
+                        {{toThousands(Math.round(scope.row.okexPosition/1000))}}
+                    </span>             
+                </template>   
+            </el-table-column>
+
+            <el-table-column label="BYBIT资金(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="">
+                        {{toThousands(Math.round(scope.row.bybitBalance/1000))}}
+                    </span>              
+                </template>
+            </el-table-column>
+
+            <el-table-column label="BYBIT仓位(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="color: green" v-if="scope.row.bybitPosition >= 0">
+                        {{toThousands(Math.round(scope.row.bybitPosition/1000))}}
+                    </span>   
+                    <span style="color: red" v-else>
+                        {{toThousands(Math.round(scope.row.bybitPosition/1000))}}
+                    </span>             
+                </template>   
+            </el-table-column>
+
+            <el-table-column label="BITGET资金(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="">
+                        {{toThousands(Math.round(scope.row.bitgetBalance/1000))}}
+                    </span>              
+                </template>
+            </el-table-column>
+
+            <el-table-column label="BITGET仓位(k)" min-width="10%" align="center">
+                <template slot-scope="scope">
+                    <span style="color: green" v-if="scope.row.bitgetPosition >= 0">
+                        {{toThousands(Math.round(scope.row.bitgetPosition/1000))}}
+                    </span>   
+                    <span style="color: red" v-else>
+                        {{toThousands(Math.round(scope.row.bitgetPosition/1000))}}
+                    </span>             
+                </template>   
+            </el-table-column>
+
+            <el-table-column label="SY持币(k)" min-width="10%" align="center">
+                <span style="color: green" v-if="sunPnl >= 0">
+                    {{sunPnl.toFixed(1)}}
+                </span>  
+                <span style="color: red" v-else>
+                    {{sunPnl.toFixed(1)}}
+                </span>            
+            </el-table-column>
+
+            <el-table-column label="YJ持币(k)" min-width="10%" align="center">
+                <span style="color: green" v-if="jiaPnl >= 0">
+                    {{jiaPnl.toFixed(1)}}
+                </span>  
+                <span style="color: red" v-else>
+                    {{jiaPnl.toFixed(1)}}
+                </span>            
+            </el-table-column>
+
+
+                   
+        </el-table>
+    </div>
+</template>
+
+<script>
+import {toThousands, getAnnualReturn, getMaxDrawdown} from '@/utils/general'
+
+
+export default {
+    props: {
+        subaccountDatas: {
+            type:Object,
+            default:{}
+        },
+        jiaPnl: {
+            type: Number,
+            default: 0
+        },
+        sunPnl: {
+            type: Number,
+            default: 0
+        },         
+    },
+
+    watch: {
+        subaccountDatas: {
+            handler(val, oldVal){
+                this.parseSubaccountDatas()
+            },
+            deep: true
+        },
+    },
+
+    data() {
+        return {
+            // 仓位
+            exchangeDatas: [{
+                binancePosition: null,
+                okexPosition: null,
+                bybitPosition: null,
+                bitgetPosition: null,
+                binanceBalance: null,
+                okexBalance: null,
+                bybitBalance: null,
+                bitgetBalance: null,
+                sunPnl: null,
+                jiaPnl: null
+            }],
+        }
+    },
+
+    created() {
+        // 分析Data
+        this.parseSubaccountDatas()
+    },
+
+    methods: {
+        parseSubaccountDatas(){  
+            // 更新平台资产和仓位
+            for(let data of this.subaccountDatas){
+                if(data.subaccount.account.name == 'binance_69174462@qq.com'){
+                    this.exchangeDatas[0].binanceBalance = data.wallet.usdt_amount
+                    this.exchangeDatas[0].binancePosition = data.positions.summary.usdt_long + data.positions.summary.usdt_short
+                }
+                if(data.subaccount.account.name == 'okex_sunyoung.capital@gmail.com'){
+                    this.exchangeDatas[0].okexBalance = data.wallet.usdt_amount
+                    this.exchangeDatas[0].okexPosition = data.positions.summary.usdt_long + data.positions.summary.usdt_short
+                }   
+                if(data.subaccount.account.name == 'bybit_475492692@qq.com'){
+                    this.exchangeDatas[0].bybitBalance = data.wallet.usdt_amount
+                    this.exchangeDatas[0].bybitPosition = data.positions.summary.usdt_long + data.positions.summary.usdt_short
+                }  
+                if(data.subaccount.account.name == 'bitget_475492692@qq.com'){
+                    this.exchangeDatas[0].bitgetBalance = data.wallet.usdt_amount
+                    this.exchangeDatas[0].bitgetPosition = data.positions.summary.usdt_long + data.positions.summary.usdt_short
+                }               
+            }
+        },
+
+        toThousands: toThousands,
+    },
+}
+
+
+</script>
