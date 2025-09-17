2fc1b1aa8ff2095b861da4eb344fa1f6ff62204f
Yang Jia <iatbst@gmail.com>
2025-09-18 01:49:53 +0800

    -

---
+++ src/api/swap_funding_rate.js
@@ -0,0 +1,16 @@
+import request from '@/utils/request'
+
+// 获取订单
+export function getSwapFundingRates(host=null, filters=null) {
+  var url = '/swap-funding-rates/?'
+  if (filters != null){
+      // 指定filters
+      url += '&' + filters
+  }
+  var req_obj = {
+    url: url,
+    method: 'get'
+  }
+  if (host) { req_obj['baseURL'] = host }
+  return request(req_obj)
+}
